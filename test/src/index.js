135186b4fb148e85871bb117c5a359e225e00f86
Koushikeya <koushikeyajangam@gmail.com>
2025-09-05 23:13:28 +0530

    add changes

--- test/src/index.js
+++
@@ -1,9 +0,0 @@
-function car(tata) {
-    return function wheel(four) {
-        return `we are returning ${tata}`
-    }
-}
-
-const one = car("tata")
-// const two = wheel("char")
-console.log(one);
